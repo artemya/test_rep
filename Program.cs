1d1e36ebf9b2c7a6e39b7bda51209ecfa3a59406
artemya <artemya@tahometer.com>
2020-08-03 16:23:32 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace test2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String message = BuildMessage();
+            Console.WriteLine(message);
+        }
+
+        static String BuildMessage() 
+        {
+            return "ss";
+        }
+    }
+}
